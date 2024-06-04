115468def1ba84277676fddc1e6131177b1bbcb8
Leniors <mokualeeroy@gmail.com>
2024-06-04 13:45:19 +0300

    all

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error):
     """ Unauthorized error handler
     """
     return jsonify({"error": "Unauthorized"}), 401
