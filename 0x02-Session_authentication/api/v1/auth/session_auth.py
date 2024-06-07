f0a35b5848328716e8eae7b07b0b734c8fb72eff
Leniors <mokualeeroy@gmail.com>
2024-06-07 03:42:56 +0100

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,11 +9,20 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
     
     def create_session(self, user_id: str = None) -> str:
-        """ Create session"""
+        """ Create session
+        """
         if user_id == None or not isinstance(user_id, str):
             return None
         else:
             key = str(uuid.uuid4())
             self.user_id_by_session_id[key] = user_id
             return key
+        
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ user id
+        """
+        if session_id == None or not isinstance(session_id, str):
+            return None
+        
+        return self.user_id_by_session_id.get(session_id)
             
