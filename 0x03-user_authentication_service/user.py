a63092d9399f404e02fe6d31c26cc75702f80036
Leniors <mokualeeroy@gmail.com>
2024-06-12 00:46:20 +0300

    User

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""" User file
+""""User file
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -7,7 +7,7 @@ from sqlalchemy import Column, Integer, String
 Base  = declarative_base()
 
 class User(Base):
-    """ Class User
+    """Class User
     """
     
     __tablename__ = 'users'
