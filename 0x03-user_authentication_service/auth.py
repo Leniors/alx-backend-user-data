bdc9978c32ce467d51c119efff850e83075b248d
Leniors <mokualeeroy@gmail.com>
2024-06-14 02:46:42 +0300

    uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,11 @@ def _hash_password(password: str) -> bytes:
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(password.encode(), salt)
         return hashed
+    
+def _generate_uuid():
+    """Generate uuid
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -48,8 +53,3 @@ class Auth:
                 return False
         except NoResultFound:
             return False
-        
-    def _generate_uuid(self):
-        """Generate uuid
-        """
-        return str(uuid.uuid4())
