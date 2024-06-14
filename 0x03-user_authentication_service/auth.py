7c13f03640ccaa68ee8616092f22a93a11f5f128
Leniors <mokualeeroy@gmail.com>
2024-06-14 05:22:32 +0300

    reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,4 +86,14 @@ class Auth:
             self._db._session.commit()
         except NoResultFound:
             return None
-                
+        
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a reset password token for a user."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError(f"User with email {email} does not exist")
+        
+        reset_token = str(uuid.uuid4())
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
